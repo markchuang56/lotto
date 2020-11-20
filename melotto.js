11262db18eacea907fcb635ba5c167b7b057eba3
jasonchuang56 <jchuang@health2sync.com>
2020-11-21 07:37:45 +0800

    拿掉多餘

--- melotto.js
+++ melotto.js
@@ -11,31 +11,17 @@ document.querySelector('button').addEventListener('click', function() {
             }
         }
     }
-  	document.getElementById("demoX").innerHTML  = arr;
+	// 取消比對
+  	//document.getElementById("demoX").innerHTML  = arr;
   	document.getElementById("val1").innerHTML  = arr[0]; 
   	document.getElementById("val2").innerHTML  = arr[1]; 
   	document.getElementById("val3").innerHTML  = arr[2]; 
   	document.getElementById("val4").innerHTML  = arr[3]; 
   	document.getElementById("val5").innerHTML  = arr[4]; 
   	document.getElementById("val6").innerHTML  = arr[5]; 
-  	document.getElementById("val7").innerHTML  = arr[6]; 
-    for (i in arr) {
+  	document.getElementById("val_b").innerHTML  = arr[6]; 
+    //for (i in arr) {
         //document.write(arr[i] + '<br>');
-    }
+    //}
 });
-
-function show() { // 單純的只是把七個號碼呈現出來而已
-	for (i=0;i<7;i++) {
-		bg = ((i!=6) ? "yball.jpg" : "bball.jpg");
-		document.write("td width=100 heigth=100 align=center valign=middle style='background-repeat:no-repat' background='"+bg+"'>");
-
-	}
-}	
-
-
-
-
-
-
-
 
