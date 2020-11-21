653fbf9155f35ca9c0cb9b5aad4f470a3a4a3b3d
jasonchuang56 <jchuang@health2sync.com>
2020-11-21 14:34:18 +0800

    clear button

--- melotto.js
+++ melotto.js
@@ -1,4 +1,13 @@
 document.querySelector('button').addEventListener('click', function() {
+	xlotto(true);
+});
+
+// Clear function
+document.querySelector('#clrdata').addEventListener('click', function() {
+	xlotto(false);
+});
+
+function xlotto(xy) {
         
     let arr = [];
     
@@ -6,22 +15,23 @@ document.querySelector('button').addEventListener('click', function() {
         while (true) {
             let num = Math.floor(Math.random() * 49) + 1;
             if (arr.indexOf(num) === -1) {
+				if(xy == true) {
                 arr.push(num);
+				} else {
+					arr.push("");
+				}
                 break;
             }
         }
     }
 	// 取消比對
   	//document.getElementById("demoX").innerHTML  = arr;
-  	document.getElementById("val1").innerHTML  = arr[0]; 
+  	document.getElementById("val1").innerHTML  = arr[0];
   	document.getElementById("val2").innerHTML  = arr[1]; 
   	document.getElementById("val3").innerHTML  = arr[2]; 
   	document.getElementById("val4").innerHTML  = arr[3]; 
   	document.getElementById("val5").innerHTML  = arr[4]; 
   	document.getElementById("val6").innerHTML  = arr[5]; 
   	document.getElementById("val_b").innerHTML  = arr[6]; 
-    //for (i in arr) {
-        //document.write(arr[i] + '<br>');
-    //}
-});
+}
 
